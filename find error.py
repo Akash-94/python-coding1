3dd8a3ec2fc5edffc15cff5f5ee261942f4726f0
Jashwanth Gowda <56092435+Jashwanth-Gowda-R@users.noreply.github.com>
2019-10-20 22:02:50 +0530

    if elif else ....

---
+++ find error.py
@@ -0,0 +1,15 @@
+a : int(input(Enter a? ))
+b : int(input(Enter b? ))
+c : int(input(Enter c? ))
+if a>b and a>c
+    print("a is largest")
+if b>a and b>c
+    print("b is largest")
+if c>a and c>b
+    print("c is largest")
+
+
+
+# 1st convert this right python program...
+
+# then convert the above program into if elif else statements
